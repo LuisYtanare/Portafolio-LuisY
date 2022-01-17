7cef777c87d1c629b7cc65ac7a69163e8d661031
LuisYtanare <gricert07alexander@gmail.com>
2022-01-17 11:16:35 -0200

    cambio de idioma

--- js/script.js
+++ js/script.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
     })
 
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Diseñador", "Freelancer", "Programador"],
+        strings: ["Developer", "Disigner", "Freelancer", "Programador"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     })
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Diseñador", "Freelancer", "Programador"],
+        strings: ["Developer", "Disigner", "Freelancer", "Programador"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
